d7b6c5c054d5d49da94e926f7fdccac90c9ff493
Fars Bein <53714581+FarsBein@users.noreply.github.com>
2020-11-14 15:42:24 -0500

    added and othe rstyle to the index about and news

--- scripts/news-api.js
+++ scripts/news-api.js
@@ -4,22 +4,32 @@ fetch('https://api.covidnow.com/v1/news/usa')
 
 
 const loopData = (data) => {
-    html=""
+    html="";
     for (const news in data) {
         if (data[news]["title"]){
+            console.log(data[news]);
             html+=
-            '<div class="card bg-light mb-3" style="max-width: 21rem;">'+
-            '<div class="card-body">'+
-              '<h5 class="card-title">'+data[news]["title"]+'</h5>'+
-              '<p class="card-text">'+'UPDATED: '+data[news]["pubDate"]+'</p>'+
+            '<a href="'+data[news]["link"]+'">'+
+            '<div class="card bg-light mb-3" style="max-width: 26rem;">'+
+            '<div class="card-body" >'+
+              '<h5 class="card-title">'+ data[news]["title"] +'</h5>'+
             '</div>'+
-            '</div>';
+            '<div class="card-body">Source: '+ data[news]["source"] +'</div>'+
+            '<div class="card-footer">'+
+                '<small class="text-muted">'+
+                'Updated: ' + data[news]["pubDate"] +'</div>'+
+                '</small>'+
+            '</div>'+
+            '</a>';
         }
     }
     const tableBody = document.querySelector('.body');
-    tableBody.innerHTML = html
+    tableBody.innerHTML = html;
 }
 
-// document.body.innerHTML = 
-// 
-// 
+// '<div class="card bg-light mb-3" style="max-width: 21rem;">'+
+// '<div class="card-body" style="background: white; border-radius: 20px;">'+
+//     data[news]["html"]+
+//   '<p class="card-text" style="font-size: 12px; margin-top: 10px;">'+'UPDATED: '+data[news]["pubDate"]+'</p>'+
+// '</div>'+
+// '</div>';
