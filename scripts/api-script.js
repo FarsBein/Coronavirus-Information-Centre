855a5b9fe21ddc960a060d1c35c6258e9d0212db
Fars Bein <53714581+FarsBein@users.noreply.github.com>
2021-01-18 20:27:31 -0500

    updated APIs

--- scripts/api-script.js
+++ scripts/api-script.js
@@ -1,27 +1,21 @@
 //Javascript script that calls a Covid-19 API that provides information regarding Covid cases
-fetch('https://api.covidnow.com/v1/global/countries')
+fetch('https://covid-api.mmediagroup.fr/v1/cases')
 .then(res => res.json())
-.then(data => loopData(data["countries"]));
-
-//Function to return information fromt the API. If there is no information then it will return 0.
-const isThereValue = (data,i,key) => {
-    if (data[i][key]) {
-        return data[i][key]
-    } else {
-        return 0
-    }
-}
+.then(data => loopData(data));
 
 //A looping funtion that populates the table in our index page.
 const loopData = (data) => {
-    console.log(data)
+    console.log("data:",data)
+    console.log("recovered:",data['Afghanistan']["All"]["recovered"])
+    console.log("confirmed:",data['Afghanistan']["All"]["confirmed"])
+    console.log("deaths:",data['Afghanistan']["All"]["deaths"])
     html=""
     //Build the HTML code for the table in our index page. Recursively runs through the data fetched from the API.
-    for (i = 0; i < data.length; i++) {
-        html+='<tr><td scope="col">'+ isThereValue(data,i,"country")+'</td>'+
-        '<td scope="col">'+ isThereValue(data,i,"recovered") +'</td>'+
-        '<td scope="col">'+ isThereValue(data,i,"confirmed") +'</td>'+
-        '<td scope="col">'+ isThereValue(data,i,"deaths") +'</td><tr>'
+    for (const country in data) {
+        html+='<tr><td scope="col">'+ country +'</td>'+
+        '<td scope="col">'+ data[country]["All"]["recovered"] +'</td>'+
+        '<td scope="col">'+ data[country]["All"]["confirmed"] +'</td>'+
+        '<td scope="col">'+ data[country]["All"]["deaths"] +'</td><tr>'
     }
     const tableBody = document.querySelector('.body');
     tableBody.innerHTML = html
--- scripts/news-api.js
+++ scripts/news-api.js
@@ -1,37 +1,34 @@
 //A javascript script that helps us populate our new page with the latest news regarding covid.
-fetch('https://api.covidnow.com/v1/news/usa')
+fetch("https://covid-19-news.p.rapidapi.com/v1/covid?q=covid&lang=en&media=True", {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-key": "4e0921712cmsh1c673f4cdbc1f7ap1ba2ddjsnc3a43cecb458",
+            "x-rapidapi-host": "covid-19-news.p.rapidapi.com"
+        }
+    })
     .then(res => res.json())
-    .then(data => loopData(data));
+    .then(data => sendToHtml(data["articles"]));
 
 //A looping function that will loop through the data and help us build the HTML cards that are displayed on the news page.
-const loopData = (data) => {
+const sendToHtml = (data) => {
     html="";
-    for (const news in data) {
-        if (data[news]["title"]){
-            console.log(data[news]);
-            //Build the HTML required to display the cards with news.
-            html+=
-            '<a href="'+data[news]["link"]+'">'+
-            '<div class="card bg-light mb-3" style="max-width: 100%; margin: 10px">'+
-            '<div class="card-body" >'+
-              '<h5 class="card-title">'+ data[news]["title"] +'</h5>'+
-            '</div>'+
-            '<div class="card-body">Source: '+ data[news]["source"] +'</div>'+
-            '<div class="card-footer">'+
-                '<small class="text-muted">'+
-                'Updated: ' + data[news]["pubDate"] +'</div>'+
-                '</small>'+
-            '</div>'+
-            '</a>';
-        }
+    for (i = 0; i < data.length; i++) {
+        console.log(data[i]);
+        //Build the HTML required to display the cards with news.
+        html+=
+        '<a href="'+data[i]["link"]+'">'+
+        '<div class="card bg-light mb-3" style="max-width: 100%; margin: 10px">'+
+        '<div class="card-body" >'+
+            '<h5 class="card-title">'+ data[i]["title"] +'</h5>'+
+        '</div>'+
+        '<div class="card-body">Source: '+ data[i]["rights"] +'</div>'+
+        '<div class="card-footer">'+
+            '<small class="text-muted">'+
+            'Updated: ' + data[i]["published_date"] +'</div>'+
+            '</small>'+
+        '</div>'+
+        '</a>';
     }
     const tableBody = document.querySelector('.body');
     tableBody.innerHTML = html;
 }
-
-// '<div class="card bg-light mb-3" style="max-width: 21rem;">'+
-// '<div class="card-body" style="background: white; border-radius: 20px;">'+
-//     data[news]["html"]+
-//   '<p class="card-text" style="font-size: 12px; margin-top: 10px;">'+'UPDATED: '+data[news]["pubDate"]+'</p>'+
-// '</div>'+
-// '</div>';
